f59b495bbd91b31f624ddd3f23dc6db658608e71
Nomad <noneprado@gmail.com>
2024-06-05 14:20:47 +0200

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+Digits to Words Converter App.
+
+Tech Stack:
+Node.js
+
+## GUIDE
+1. `npm i` - to install libraries
+2. `npm start` - to run app
---
+++ src/helper/digitsConverter.mjs
@@ -0,0 +1,36 @@
+import { ones, tens, teens, zero, hundred, thousand, million, MILLION_DIGIT, THOUSAND_DIGIT, HUNDRED_DIGIT, TEN_DIGIT, TWENTY_DIGIT, NINETY_NINE_DIGIT, ZERO_DIGIT } from '../constants.mjs'
+
+const convertMillions = (number) => {
+    if (number >= MILLION_DIGIT) {
+        return convertMillions(Math.floor(number / MILLION_DIGIT)) + million + convertThousands(number % MILLION_DIGIT);
+    }
+
+    return convertThousands(number);
+};
+
+const convertThousands = (number) => {
+    if (number >= THOUSAND_DIGIT) {
+        return convertHundreds(Math.floor(number / THOUSAND_DIGIT)) + thousand + convertHundreds(number % THOUSAND_DIGIT);
+    }
+
+    return convertHundreds(number);
+};
+
+const convertHundreds = (number) => {
+    if (number > NINETY_NINE_DIGIT) {
+        return ones[Math.floor(number / HUNDRED_DIGIT)] + hundred + convertTens(number % HUNDRED_DIGIT);
+    }
+
+    return convertTens(number);
+};
+
+const convertTens = (number) => {
+    if (number < TEN_DIGIT) return ones[number];
+    if (number >= TEN_DIGIT && number < TWENTY_DIGIT) return teens[number - TEN_DIGIT];
+    return tens[Math.floor(number / TEN_DIGIT)] + " " + ones[number % TEN_DIGIT];
+};
+
+const convert = (number) => number == ZERO_DIGIT ? zero : convertMillions(number);
+
+export const getNumbers = (number) => number ? convert(number) : null;
+
